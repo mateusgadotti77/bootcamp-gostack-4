d39ed3d54f4d43f8f2262bfeee6b1b60fcc5ac61
Qualix <maateusgadotti@gmail.com>
2020-02-10 17:01:28 -0300

    Searching data in API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,47 @@
-import React from 'react';
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
-          alt="Tênis"
-        />
-        <strong>Nike Air Jordan 1</strong>
-        <span>R$990,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
