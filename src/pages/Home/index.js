da65894e6bef4637ae1287aa063a83251803cead
Qualix <maateusgadotti@gmail.com>
2020-02-10 15:38:46 -0300

    Create header and styling home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://artwalk.vteximg.com.br/arquivos/ids/219519-380-380/Tenis-Nike-Air-Jordan-1-Mid-Masculino-Preto.jpg?v=637136682643300000"
+          alt="Tênis"
+        />
+        <strong>Nike Air Jordan 1</strong>
+        <span>R$990,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
