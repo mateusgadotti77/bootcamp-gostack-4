234c477bb91f8613d7778a4b7d74b27c981f091a
Qualix <maateusgadotti@gmail.com>
2020-02-12 16:34:55 -0300

    Reactotron configuration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
+
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
